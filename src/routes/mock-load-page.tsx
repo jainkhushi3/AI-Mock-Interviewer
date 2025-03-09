6b2b907f3ec90201c3126c7b867d54dde4bfd932
jainkhushi3 <162288022+jainkhushi3@users.noreply.github.com>
2025-03-09 21:54:28 +0530

    Update mock-load-page.tsx

--- src/routes/mock-load-page.tsx
+++ src/routes/mock-load-page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Interview } from "@/types";
 import { doc, getDoc } from "firebase/firestore";
